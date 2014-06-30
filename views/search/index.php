ca9d83a5846c87cf64931d331beb1bebd4e1292f
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-06-30 09:25:48 +0200

    include search in form

--- views/search/index.php
+++ views/search/index.php
@@ -2,9 +2,9 @@
 <h1><?= dgettext('supportplugin', 'Suche') ?></h1>
 <form class="studip_form" action="<?= $controller->url_for('search/do_search') ?>" method="post">
     <label class="caption" for="searchterm">
-        <?= dgettext('supportplugin', 'Suchbegriff:') ?>
+        <?= dgettext('supportplugin', 'Suche nach Veranstaltungen, Personen und Einrichtungen:') ?>
     </label>
-    <input type="text" name="searchterm" value="<?= $searchterm ? htmlReady($searchterm) : '' ?>" size="75" maxlength="255" placeholder="<?= dgettext('supportplugin', 'Geben Sie hier Ihren Suchbegriff ein.') ?>"/>    
+    <?= $search ?>
     <?= CSRFProtection::tokenTag() ?>
     <?= Button::createAccept(dgettext('supportplugin', 'Suchen'), 'submit') ?>
 </form>
