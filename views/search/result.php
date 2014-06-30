1a0ad2b6b280d133a8e47524a1ab5e88106d5218
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-06-30 16:10:48 +0200

    display search results

--- views/search/result.php
+++ views/search/result.php
@@ -1,33 +1,12 @@
 <h1><?= dgettext('supportplugin', 'Suchergebnisse') ?></h1>
 <?= $this->render_partial('search/_search') ?>
-<?php if ($courses) { ?>
-<table class="default">
-    <caption><?= dgettext('supportplugin', 'Veranstaltungen') ?></caption>
-    <thead>
-        <tr>
-            <th><?= dgettext('supportplugin', 'Nummer') ?></th>
-            <th><?= dgettext('supportplugin', 'Name') ?></th>
-            <th><?= dgettext('supportplugin', 'Info') ?></th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($courses as $c) { ?>
-        <tr data-id="<?= $c->id ?>">
-            <td><?= htmlReady($c->veranstaltungsnummer) ?></td>
-            <td><?= htmlReady($u->name) ?></td>
-            <td></td>
-        </tr>
-        <?php } ?>
-    </tbody>
-</table>
-<?php } ?>
 <?php if ($persons) { ?>
 <table class="default">
     <caption><?= dgettext('supportplugin', 'Personen') ?></caption>
     <thead>
         <tr>
             <th><?= dgettext('supportplugin', 'Name') ?></th>
-            <th><?= dgettext('supportplugin', 'Kennung') ?></th>
+            <th><?= dgettext('supportplugin', 'Nutzername') ?></th>
             <th><?= dgettext('supportplugin', 'Info') ?></th>
         </tr>
     </thead>
@@ -36,6 +15,29 @@
         <tr data-id="<?= $u->id ?>">
             <td><?= htmlReady($u->getFullname()) ?></td>
             <td><?= htmlReady($u->username) ?></td>
+            <td></td>
+        </tr>
+        <?php } ?>
+    </tbody>
+</table>
+<?php } ?>
+<?php if ($courses) { ?>
+<table class="default">
+    <caption><?= dgettext('supportplugin', 'Veranstaltungen') ?></caption>
+    <thead>
+        <tr>
+            <th><?= dgettext('supportplugin', 'Semester') ?></th>
+            <th><?= dgettext('supportplugin', 'Nummer') ?></th>
+            <th><?= dgettext('supportplugin', 'Name') ?></th>
+            <th><?= dgettext('supportplugin', 'Info') ?></th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($courses as $c) { ?>
+        <tr data-id="<?= $c->id ?>">
+            <td><?= htmlReady($c->start_semester->name) ?></td>
+            <td><?= htmlReady($c->veranstaltungsnummer) ?></td>
+            <td><?= htmlReady($c->name) ?></td>
             <td></td>
         </tr>
         <?php } ?>
@@ -54,7 +56,7 @@
     <tbody>
         <?php foreach ($institutes as $i) { ?>
         <tr data-id="<?= $i->id ?>">
-            <td><?= htmlReady($u->name) ?></td>
+            <td><?= htmlReady($i->name) ?></td>
             <td></td>
         </tr>
         <?php } ?>
