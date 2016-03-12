d1b07d920a090e3f65d62ea3559e8eb1d2a42fff
hackl <thomas.hackl@uni-passau.de>
2016-03-12 10:48:56 +0100

    use default form class

--- views/search/_search.php
+++ views/search/_search.php
@@ -1,14 +1,14 @@
 <?php use Studip\Button; ?>
-<form action="<?= $controller->url_for('search/result') ?>" method="post" id="supportsearch" data-redirect-url="<?= $controller->url_for('search/redirect') ?>">
+<form class="default" action="<?= $controller->url_for('search/result') ?>" method="post" id="supportsearch" data-redirect-url="<?= $controller->url_for('search/redirect') ?>">
     <?= $search ?>
     <label for="semester">
         <?= dgettext('supportplugin', 'Semester für Veranstaltungen:') ?>
+        <select name="semester">
+            <option value=""><?= dgettext('supportplugin', 'alle') ?></option>
+            <?php foreach (array_reverse(Semester::getAll()) as $semester) { ?>
+                <option value="<?= $semester->id ?>"<?= $semester->id==$selected_semester ? ' selected="selected"' : '' ?>><?= htmlReady($semester->name) ?></option>
+            <?php } ?>
+        </select>
     </label>
-    <select name="semester">
-        <option value=""><?= dgettext('supportplugin', 'alle') ?></option>
-        <?php foreach (array_reverse(Semester::getAll()) as $semester) { ?>
-        <option value="<?= $semester->id ?>"<?= $semester->id==$selected_semester ? ' selected="selected"' : '' ?>><?= htmlReady($semester->name) ?></option>
-        <?php } ?>
-    </select>
     <?= CSRFProtection::tokenTag() ?>
 </form>
