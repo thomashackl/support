a962168169c004ac0f246f597016de63bbeb647b
hackl <thomas.hackl@uni-passau.de>
2017-09-19 16:46:37 +0200

    language files

--- extractTranslationStrings.sh
+++ extractTranslationStrings.sh
@@ -10,7 +10,8 @@ TRANSLATIONFILES_RELATIVE_PATHS="."
 
 for language in en
 do
-    test -f "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/supportplugin.po" && mv "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/supportplugin.po" "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/supportplugin.po.old" > "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/supportplugin.po"
+    test -f "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/supportplugin.po" && mv "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/supportplugin.po" "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/supportplugin.po.old"
+    > "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/supportplugin.po"
     find $TRANSLATIONFILES_RELATIVE_PATHS \( -iname "*.php" \) | xargs xgettext --from-code=UTF-8 -j -n --language=PHP -o "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/supportplugin.po"
     test -f "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/supportplugin.po.old" && msgmerge "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/supportplugin.po.old" "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/supportplugin.po" --output-file="$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/supportplugin.po"
 done
